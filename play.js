8fe4f69637a9ccc7b550e2c836be93f3a0477718
hansori01 <19sorihan88@gmail.com>
2021-02-10 20:37:27 +0000

    refactored serverside code

--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-const net = require('net');
 
-// establishes connection with the game server
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542
-  });
-  //interpret incoming data as text
-  conn.setEncoding('utf8');
-  // message from server
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  return conn;
-}
+const connect = require('./client');
 console.log('Connecting...');
 connect();
