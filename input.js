d49fa5f8d9412bc83450015140e3957191301873
hansori01 <19sorihan88@gmail.com>
2021-02-10 22:22:40 +0000

    added up down left right console logs to test input stdin.on

--- input.js
+++ input.js
@@ -3,12 +3,29 @@ const setupInput = () => {
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
+  // handleUserInput();
   return stdin;
 };
 const handleUserInput = stdin.on('data', (key) => {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w'){
+    console.log('up')
+  };
+  if (key === 's'){
+    console.log('down')
+  };
+  if (key === 'a'){
+    console.log('left')
+  };
+  if (key === 'd'){
+    console.log('right')
+  };
+
 });
+
+
+
 
 module.exports = {setupInput};
