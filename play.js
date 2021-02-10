81e95d5a4b0cbca58535871ed14ca91c4d7fe19f
hansori01 <19sorihan88@gmail.com>
2021-02-10 21:59:27 +0000

    added keyboard input syntax and ability to quit game with control + c

--- play.js
+++ play.js
@@ -2,3 +2,18 @@
 const {connect} = require('./client');
 console.log('Connecting...');
 connect();
+
+const stdin = process.stdin;
+const setupInput = () => {
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+const handleUserInput = stdin.on('data', (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+});
+
+setupInput();
