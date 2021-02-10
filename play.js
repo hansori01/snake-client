9b3a3f5c19316478ab4224d6e2e8cb1110193781
hansori01 <19sorihan88@gmail.com>
2021-02-10 20:09:52 +0000

    added server syntax to connect to server

---
+++ play.js
@@ -0,0 +1,15 @@
+const net = require('net');
+
+// establishes connection with the game server
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50451
+  });
+  //interpret incoming data as text
+  conn.setEncoding('utf8');
+  return conn;
+}
+console.log('Connecting...');
+connect();
